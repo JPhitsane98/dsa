4f2c2e1049d241ceb19bc9050ef767a529a9a0cf
JPhitsane98 <selloanephitsane@gmail.com>
2024-11-04 15:38:17 +0200

    refactored

--- problems/j/com/problems/TwoSum.java
+++ problems/j/com/problems/TwoSum.java
@@ -22,8 +22,8 @@ class TwoSum {
     public static int[] twoSumArrayIteration(int[] nums, int target) {
         //O(n2)
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target){
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
             }
@@ -36,7 +36,7 @@ class TwoSum {
         Map<Integer, Integer> mapNums = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if(mapNums.containsKey(complement)){
+            if (mapNums.containsKey(complement)) {
                 return new int[]{mapNums.get(complement), i};
             }
             mapNums.put(nums[i], i);
@@ -44,40 +44,29 @@ class TwoSum {
         return new int[0];
     }
 
-    private static void testExpectedResult0and1(){
-        Logger log = getLogger();
-        int[] nums = new int[]{2,7,11,15};
-        int target = 9;
-        log.log(Level.INFO, "twoSumArrayIteration: " + Arrays.toString(twoSumArrayIteration(nums, target)) +
-                "\ntwoSumMap: " + Arrays.toString(twoSumMap(nums, target)));
+    private static void testExpectedResult0and1() {
+        logResults(getLogger(), new int[]{2, 7, 11, 15}, 9);
     }
 
-    private static void testExpectedResult1and2(){
-        Logger log = getLogger();
-        int[] nums = new int[]{3,2,4};
-        int target = 6;
-        log.log(Level.INFO, "twoSumArrayIteration: " + Arrays.toString(twoSumArrayIteration(nums, target)) +
-                "\ntwoSumMap: " + Arrays.toString(twoSumMap(nums, target)));
+    private static void testExpectedResult1and2() {
+        logResults(getLogger(), new int[]{3, 2, 4}, 6);
     }
 
-    private static void testExpectedResult0and3(){
-        Logger log = getLogger();
-        int[] nums = new int[]{1,5,9,13};
-        int target = 14;
-        log.log(Level.INFO, "twoSumArrayIteration: " + Arrays.toString(twoSumArrayIteration(nums, target)) +
-                "\ntwoSumMap: " + Arrays.toString(twoSumMap(nums, target)));
+    private static void testExpectedResult0and3() {
+        logResults(getLogger(), new int[]{1, 5, 9, 13}, 14);
     }
 
-    private static void testExpectedResult3and4(){
-        Logger log = getLogger();
-        int[] nums = new int[]{1,3,7,9,2};
-        int target = 11;
-        log.log(Level.INFO, "twoSumArrayIteration: " + Arrays.toString(twoSumArrayIteration(nums, target)) +
-                "\ntwoSumMap: " + Arrays.toString(twoSumMap(nums, target)));
+    private static void testExpectedResult3and4() {
+        logResults(getLogger(), new int[]{1, 3, 7, 9, 2}, 11);
     }
 
     private static Logger getLogger() {
         return Logger.getLogger(TwoSum.class.getName());
+    }
+
+    private static void logResults(Logger log, int[] nums, int target) {
+        log.log(Level.INFO, "twoSumArrayIteration: " + Arrays.toString(twoSumArrayIteration(nums, target)) +
+                "\ntwoSumMap: " + Arrays.toString(twoSumMap(nums, target)));
     }
 
 }
